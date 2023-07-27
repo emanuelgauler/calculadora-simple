d952ed1c443628a60a669795dde6ee3aa2b56c78
emanuelgauler <emanuelgauler@gmail.com>
2023-07-26 22:13:00 -0300

    primer etapa finalizada

---
+++ calculator.js
@@ -0,0 +1,29 @@
+var buttons = document.querySelectorAll("button");
+
+function calculate(params) {
+    if( params.length === 0 ) return "";
+    return eval( params );
+}
+
+for (const button of buttons) {
+
+    if (button.innerHTML === "=") {
+        button.addEventListener('click', function (e) {
+            pantalla.innerHTML = calculate(pantalla.innerHTML);
+        }, false);
+    } else if (button.dataset.value === "AC") {
+        button.addEventListener( 'click', function(e) {
+            pantalla.innerHTML = "";
+        }, false);
+    } else if( button.dataset.value === "C" ) {
+        button.addEventListener( 'click', function(e) {
+            let letters = pantalla.innerHTML.split("");
+            letters.pop();
+            pantalla.innerHTML = letters.join("");
+        }, false);
+    } else {
+        button.addEventListener('click', function (e) {
+            pantalla.innerHTML += button.dataset.value;
+        }, false);
+    }
+}
---
+++ string_test.js
@@ -0,0 +1,8 @@
+let msg = "qwerty";
+let splitted = msg.split("");
+console.log( splitted );
+
+splitted.pop();
+let joined = splitted.join("");
+
+console.log( joined );
